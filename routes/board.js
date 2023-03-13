7615b20736279fa4bf3cdf58aa3c29af6e4d1051
Drew <drc920630@gmail.com>
2023-03-13 15:49:09 +0900

    게시판 사제까지 완료

--- routes/board.js
+++ routes/board.js
@@ -26,23 +26,17 @@ router.get('/write', (req, res) => {
 //글 추가 기능
 router.post('/write', (req, res) => {
   console.log(Object.keys(req.body).length);
-  if (Object.keys(req.body).length >= 1) {
-    console.log(req.body.title);
-    console.log(req.body.content);
-    if (req.body.title && req.body.content) {
-      const newWrite = {
-        title: req.body.title,
-        content: req.body.content,
-      };
-      ARTICLE.push(newWrite);
-      res.redirect('/board');
-    } else {
-      const err = new Error('모든 정보를 입력해주세요.');
-      err.statusCode = 400;
-      throw err;
-    }
+  console.log(req.body.title);
+  console.log(req.body.content);
+  if (req.body.title && req.body.content) {
+    const newWrite = {
+      title: req.body.title,
+      content: req.body.content,
+    };
+    ARTICLE.push(newWrite);
+    res.redirect('/board');
   } else {
-    const err = new Error('데이터가 없습니다. 데이터오류');
+    const err = new Error('모든 정보를 입력해주세요.');
     err.statusCode = 400;
     throw err;
   }
@@ -52,12 +46,39 @@ router.post('/write', (req, res) => {
 
 //글 수정
 //글 수정 모드로 이동
-router.get('/modify/:title', (req, res) => {});
-router.post('/modify/:title', (req, res) => {});
+router.get('/modify/:title', (req, res) => {
+  const arrIndex = ARTICLE.findIndex(
+    (article) => req.params.title === article.title
+  );
+  const selectedArticle = ARTICLE[arrIndex];
+  res.render('board_modify', { selectedArticle });
+});
+
+//글 수정 후 추가
+router.post('/modify/:title', (req, res) => {
+  if (req.body.title && req.body.content) {
+    const arrIndex = ARTICLE.findIndex(
+      (article) => req.params.title === article.title
+    );
+    ARTICLE[arrIndex].title = req.body.title;
+    ARTICLE[arrIndex].content = req.body.content;
+    res.redirect('/board');
+  } else {
+    const err = new Error('모든 정보를 입력해주세요.');
+    err.statusCode = 400;
+    throw err;
+  }
+});
 
 //--------------------------
 
 //글삭제
-router.delete('/delete/:title', (req, res) => {});
+router.delete('/delete/:title', (req, res) => {
+  const arrIndex = ARTICLE.findIndex(
+    (article) => article.title === req.params.title
+  );
+  ARTICLE.splice(arrIndex, 1);
+  res.send('삭제완료!');
+});
 
 module.exports = router;
