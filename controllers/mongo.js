6ac61db1f6abe6ee371a399aafabb2244b173190
Drew <drc920630@gmail.com>
2023-03-20 15:32:07 +0900

    몽고고db연결 성공

---
+++ controllers/mongo.js
@@ -0,0 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+  'mongodb+srv://drc920630:drew1234@cluster0.ikcoamt.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const test = client.db('kdt5').collection('test');
+  test.deleteMany({}, (deleteErr, deleteResult) => {
+    //첫인자로 조건, 두번째로 콜백인자(에러, 결과값)
+    if (deleteErr) throw deleteErr;
+    console.log(deleteResult);
+    test.insertOne(
+      {
+        name: 'tetz',
+        nickName: 'chicken head',
+      },
+      (insertErr, insertResult) => {
+        console.log(insertResult);
+        client.close();
+      },
+    );
+  });
+});
